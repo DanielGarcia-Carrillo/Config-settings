8eace222c361afdf52ae0894ba11a8721ef0c801
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2015-02-20 13:05:01 -0800

    Devbox setup

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/DanielGarcia-Carrillo/Config-settings/master/.bashrc https://raw.githubusercontent.com/DanielGarcia-Carrillo/Config-settings/master/.vimrc https://raw.githubusercontent.com/DanielGarcia-Carrillo/Config-settings/master/.gitconfig
+
+cat ~/.bashrc.1 >> ~/.bashrc
